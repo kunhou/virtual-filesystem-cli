7f336aaf6086db041a52f5a98a0f3c55d06747ba
Patrice <kunhoulee@gmail.com>
2023-09-21 12:10:43 +0800

    Add file operations to repository interface and introduce new usecase functions

--- internal/usecase/interface.go
+++ internal/usecase/interface.go
@@ -11,4 +11,18 @@ type IRepository interface {
 	CreateFolder(username string, folder entity.Folder) error
 	DeleteFolder(username, folderName string) error
 	ListFolders(username string, opt entity.ListFolderOption) ([]*entity.Folder, error)
+
+	CreateFile(username string, folderName string, file entity.File) error
+	DeleteFile(username, folderName, fileName string) error
+	ListFiles(username, folderName string, opt entity.ListFileOption) ([]*entity.File, error)
+}
+
+type Usecase struct {
+	repo IRepository
+}
+
+func NewUsecase(repo IRepository) *Usecase {
+	return &Usecase{
+		repo: repo,
+	}
 }
