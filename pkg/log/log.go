b39960d1748789c5080078b177ce7898f08c420b
Patrice <kunhoulee@gmail.com>
2023-09-21 14:21:05 +0800

    Add custom logging package for STDOUT and STDERR

---
+++ pkg/log/log.go
@@ -0,0 +1,21 @@
+package log
+
+import (
+	"fmt"
+	"os"
+)
+
+// Info logs a message to stdout.
+func Info(format string, a ...interface{}) {
+	fmt.Fprintf(os.Stdout, "INFO: "+format+"\n", a...)
+}
+
+// Warn logs a warning message to stdout.
+func Warn(format string, a ...interface{}) {
+	fmt.Fprintf(os.Stdout, "WARNING: "+format+"\n", a...)
+}
+
+// Error logs an error message to stderr.
+func Error(format string, a ...interface{}) {
+	fmt.Fprintf(os.Stderr, "ERROR: "+format+"\n", a...)
+}
